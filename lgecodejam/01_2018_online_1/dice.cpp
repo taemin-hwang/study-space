86dee637f81d938ccf124c5144c79aea789b89aa
taemin.hwang <taemin.hwang@lge.com>
2020-03-31 13:47:23 +0900

    Fix indent of Stopwatch source code

--- lgecodejam/01_2018_online_1/dice.cpp
+++ lgecodejam/01_2018_online_1/dice.cpp
@@ -10,14 +10,16 @@ int n;
 vector<int> dice;
 int cache[1001][1001];
 int visit[1001];
-
+bool isFound = false;
 int dicesol(int start, int end, int num, int dices) {
-    int& ret = cache[start][end];
-    if(num != -1 && num < dices) return ret = 0;
-    if(dices == 0) return ret = 1;
-    if(ret != -1) return ret;
+    if(isFound) return 1;
+    if(num != -1 && num < dices) return 0;
+    if(dices == 0) {
+        isFound = true;
+        return 1;
+    }
 
-    ret = 0;
+    int ret = 0;
     for(int i = start ; i < end; i++) {
         if(visit[i] != 0) continue;
         visit[i] = 1;
@@ -28,13 +30,15 @@ int dicesol(int start, int end, int num, int dices) {
 }
 
 int mindicesol(int start, int end) {
+    int& ret = cache[start][end];
     if(start+1 == end) return 1;
+    isFound = false;
     if(dicesol(start, end, -1, end-start)) return 1;
+    if(ret != -1) return ret;
 
-    int ret = INT_MAX;
-    for(int i = start; i < end-1; i++) {
-        ret = min(ret, mindicesol(start, i+1) + mindicesol(i+1, end));
-    }
+    ret = INT_MAX;
+    int idx = (start + end)/2;
+    ret = min(ret, mindicesol(start, idx+1) + mindicesol(idx+1, end));
     return ret;
 }
 
@@ -45,6 +49,7 @@ int main() {
         cin >> dice[i];
     }
     memset(cache, -1, sizeof(cache));
+    sort(dice.begin(), dice.end());
     cout << mindicesol(0, n) << endl;
 }
 
