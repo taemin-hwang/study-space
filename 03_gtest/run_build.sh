f6bd410df56ce66e714d3fbdd3cb02b8f05a19a1
taemin.hwang <taeminee@kakao.com>
2020-05-19 15:51:14 +0900

    Create build directory in shell script

--- 03_gtest/run_build.sh
+++ 03_gtest/run_build.sh
@@ -2,6 +2,10 @@
 
 working_dir=${PWD}/
 
+if [ ! -d $working_dir/build ]
+    then mkdir $working_dir/build
+fi
+
 pushd build
 cmake -DCMAKE_INSTALL_BINDIR=${PWD}/.. -DENABLE_TEST=ON -DWORKING_DIR=$working_dir ..
 make
