ab1435554a633cbb27d29721975c3f8f5cf79cf9
taemin <taeminee@kakao.com>
2022-07-06 16:24:44 +0900

    Add an example of asyncio returning output

--- python/99_self/async/main.py
+++ python/99_self/async/main.py
@@ -4,14 +4,18 @@ import asyncio
 async def good_night(message):
     await asyncio.sleep(1)
     print(message)
+    return "thank you, " + message
 
-async def main():
-    await asyncio.gather(
-        good_night('good'),
-        good_night('night')
+async def main(messages):
+    ret = await asyncio.gather(
+        good_night(messages[0]),
+        good_night(messages[1])
     )
+    return ret
 
 for i in range(10):
     print(f"start : {time.strftime('%X')}")
-    asyncio.run(main())
+    messages = ["good", "night"]
+    ret = asyncio.run(main(messages))
+    print(ret)
     print(f"end : {time.strftime('%X')}")
